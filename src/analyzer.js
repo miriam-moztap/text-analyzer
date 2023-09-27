acf0f57bbe6e157a92377673014f9f932aa982fc
miriam-moztap <ada_hack16@hackwomen.dev>
2023-09-26 18:45:11 -0600

    se crean funciones para crear contador de palabras, contador de caracteres, contador de caracteres sin espacios y contador de números. se agrega el botón para borrar el texto y el evento de escucha keyup así como el código de js para invocar las funciones del analyzer

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,17 +1,59 @@
 const analyzer = {  
   getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  /*const wordsStr=text.value;
+  const words=wordsStr.split(" ");
+  return words.length;*/
+
+    const strText = text.value; /*si no utilizo el .value, el programa llama al texto como un objeto y no como un str*/
+    const words=strText.split(' ');
+    const wordsList=[];
+    let counter=0;
+    for (let word of words){
+      if (word !== ' ') {
+      wordsList.push(word);
+      counter=counter+1;
+      }
+    }
+  return counter-1; /*necesito optimizar este código ya que cuenta el espacio como una palabra, por eso el -1*/
   },
   getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    const wordsStr=text.value;
+    return wordsStr.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
+    const characterList=['!'," ",'|','¬','"','#','$','%','&','/','(',')','=','?',
+    '¡','¿','*','+','-','~','´','{','}','[',']','`','^','_','.',';',',','@','>','<'];
+    let onlyLetters=[];
+    //let counterLetters=0;
+    const strText = text.value; /*si no utilizo el .value, el programa llama al texto como un objeto y no como un str*/
+    const letters=strText.split('');
+    for(const letter of letters){
+      if (!characterList.includes(letter) && letter !== ' '){
+        onlyLetters.push(letter);
+        //counterLetters=counterLetters+1;
+      }
+    }
+  //return counterLetters-1;
+    return onlyLetters.length;
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {   2 
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberCount: (text) => {
+    const strText = text.value;  
+    const fullText=strText.split('');
+    const numberList=['1','2','3','4','5','6','7','8','9','0'];
+    let counterNumbers=0;
+
+    for(let char of fullText){
+      if(numberList.includes(char)){
+        counterNumbers=counterNumbers+1;
+
+      }
+    }
+    return counterNumbers;
+    
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberSum: (text) => {
--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import analyzer from './analyzer.js';
 
+
+    const textArea=document.querySelector('textarea[name="user_input"]');
+    const clearbutton=document.getElementById("reset-button");
+    clearbutton.addEventListener("click", () => {    
+    textArea.value=''
+    document.querySelector('[data-testid="word-count"]').innerHTML='palabras: ' + 0;
+    document.querySelector('[data-testid="character-count"]').innerHTML='caracteres: ' + 0;
+    document.querySelector('[data-testid="character-no-spaces-count"]').innerHTML='caracteres sin espacios: ' +0;
+    document.querySelector('[data-testid="number-count"]').innerHTML='números: ' + 0;
+    document.querySelector('[data-testid="number-sum"]').innerHTML='suma de números: ' + 0;
+    document.querySelector('[data-testid="word-length-average"]').innerHTML='promedio por palabra: ' + 0;
+
+    });
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+
+    textArea.addEventListener('keyup', function(){
+    const text=document.querySelector('textarea[name="user_input"]');
+    document.querySelector('[data-testid="word-count"]').innerHTML='palabras: ' + analyzer.getWordCount(text);
+    document.querySelector('[data-testid="character-count"]').innerHTML='caracteres: ' + analyzer.getCharacterCount(text);
+    document.querySelector('[data-testid="character-no-spaces-count"]').innerHTML='caracteres sin espacios: ' + analyzer.getCharacterCountExcludingSpaces(text);
+    document.querySelector('[data-testid="number-count"]').innerHTML='pero ahí vamos!';
+    document.querySelector('[data-testid="number-sum"]').innerHTML='números: ' + analyzer.getNumberCount(text);
+    document.querySelector('[data-testid="word-length-average"]').innerHTML='A por ellooooo';
+
+    });
+
