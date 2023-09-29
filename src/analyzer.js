b1e39a746810509a82ed91394e922bee50c6d5c7
miriam-moztap <ada_hack16@hackwomen.dev>
2023-09-29 10:21:58 -0600

    se corrigen errores de las funciones. Se completan las funciones

--- src/analyzer.js
+++ src/analyzer.js
@@ -2,63 +2,117 @@ const analyzer = {
   getWordCount: (text) => {
   /*const wordsStr=text.value;
   const words=wordsStr.split(" ");
-  return words.length;*/
+  return words.length;*/ 
 
-    const strText = text.value; /*si no utilizo el .value, el programa llama al texto como un objeto y no como un str*/
-    const words=strText.split(' ');
-    const wordsList=[];
+    
+    const strText = text.trim().split(" ");
+    
+    if (text==="" || text===" "){
+      return 0;
+    }
     let counter=0;
-    for (let word of words){
-      if (word !== ' ') {
-      wordsList.push(word);
-      counter=counter+1;
+    for (let i=0; i<strText.length; i++){ 
+      if(strText.length>0) {
+        counter++;
       }
     }
-  return counter-1; /*necesito optimizar este código ya que cuenta el espacio como una palabra, por eso el -1*/
+    return Number(counter); 
   },
   getCharacterCount: (text) => {
-    const wordsStr=text.value;
+    if (text===" "){
+      return 0;
+    }
+    const wordsStr=text;
     return wordsStr.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
+    if (text==="" || text===" "){
+      return 0;
+    }
     const characterList=['!'," ",'|','¬','"','#','$','%','&','/','(',')','=','?',
-    '¡','¿','*','+','-','~','´','{','}','[',']','`','^','_','.',';',',','@','>','<'];
-    let onlyLetters=[];
-    //let counterLetters=0;
-    const strText = text.value; /*si no utilizo el .value, el programa llama al texto como un objeto y no como un str*/
-    const letters=strText.split('');
+      '¡','¿','*','+','-','~','´','{','}','[',']','`','^','_','.',';',',','@','>','<'];
+    const onlyLetters=[]; 
+    const letters=text.split('');
     for(const letter of letters){
       if (!characterList.includes(letter) && letter !== ' '){
         onlyLetters.push(letter);
-        //counterLetters=counterLetters+1;
       }
     }
-  //return counterLetters-1;
     return onlyLetters.length;
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getAverageWordLength: (text) => {   2 
+  getAverageWordLength: (text) => {   
+    
+    
+    if (text==="" || text===" "){
+      return 0;
+    }
+    // Dividir el texto en palabras, teniendo en cuenta signos de puntuación
+    /*const palabras = text.split(" ");
+
+    if (!palabras) {
+      return 0; // En caso de que no haya palabras válidas en el texto
+    }
+
+    // Calcular la suma de las longitudes de todas las palabras
+    const sumaLongitudes = palabras.reduce((acumulador, palabra) => {
+      return acumulador + palabra.length;
+    }, 0);
+
+    // Calcular el largo promedio
+    const largoPromedio = sumaLongitudes / palabras.length;
+
+    return largoPromedio.toFixed(2);*/
+
+    const wordsA=text.split(/\s+/g);
+    let charsPerWord=0;
+    for(let i=0; i<wordsA.length; i++){
+      const word= wordsA[i];
+      charsPerWord=charsPerWord+word.length;
+    }
+    const average=((charsPerWord)/(wordsA.length)).toFixed(2);
+    return (parseFloat(average)); //recuerda que siempre se retorna un string, por eso el parseFloat va en el retorno final
+
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberCount: (text) => {
-    const strText = text.value;  
-    const fullText=strText.split('');
-    const numberList=['1','2','3','4','5','6','7','8','9','0'];
-    let counterNumbers=0;
-
-    for(let char of fullText){
-      if(numberList.includes(char)){
-        counterNumbers=counterNumbers+1;
-
-      }
+    if (text===" "){
+      return 0;
     }
-    return counterNumbers;
-    
+    const Texto= text.match(/[-+]?\b\d+(\.\d+)?\b/g); // Encuentra todos los números en el texto que no estén unidos a letras, decimales y positivos y negativos
+    const numsList=[]; 
+    if (Texto) {
+      numsList.push(...Texto.map(Number)); // agregar los valores encontrados al array
+    }
+    let counter=0;
+    for (const dato of numsList){
+      if(numsList.includes(dato)){
+        counter=counter+1;
+      }
+      
+    }
+    return(counter);
+  
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberSum: (text) => {
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-  },
+    if (text===" "){
+      return 0;
+    }
+    const Texto= text.match(/[-+]?\b\d+(\.\d+)?\b/g); // Encuentra todos los números en el texto que no estén unidos a letras
+    const numsList=[];
+    
+    if (Texto) {
+      numsList.push(...Texto.map(Number)); // agregarlos valores encontrados al array
+    }
+    let counter=0;
+    for (const dato of numsList){
+      
+      counter=counter+dato;
+    }
+    return(counter);
+  }
+
 };
 
 export default analyzer;
